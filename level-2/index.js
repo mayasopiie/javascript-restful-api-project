9ce7488b7cf44101aa00d2489c5bbec956f1d88b
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-08-01 09:12:07 +0700

    DIsplay Star Wars characters with 'for' (bug fixed)

--- level-2/index.js
+++ level-2/index.js
@@ -4,25 +4,25 @@ fetch("https://swapi.co/api/people")
     })
     .then(data => {
         const peopleArray = data.results;
-        peopleArray.forEach(function(person) {
+        peopleArray.forEach(function (person) {
             console.log(`Name: ${person.name}`);
             console.log(`Height: ${person.height}`);
             console.log(`Hair color: ${person.hair_color}`);
             console.log("-------")
         });
+        
+        // peopleArray.map((person) => {
+        //     debugger
+        //     const nameDisplay = `<h3>Name: ${person.name}</h3>`;
+        //     const peopleDisplay = document.getElementById("people-display");
+        //     peopleDisplay.innerHTML += nameDisplay;
+        // });
 
-        const display = function(){
-            peopleArray.map((person, index) => {
-                let nameNode = document.createElement("p");
-                let nameText = document.createTextNode(person[index].name);
-                nameNode.appendChild(nameText);
-    
-                let cluster = document.getElementById("people-display");
-                cluster.appendChild(nameNode);
-            });
-        };
-
-        display(); 
+        const peopleDisplay = document.getElementById("people-display");
+        for(let i=0; i < peopleArray.length; i++){
+            peopleDisplay.innerHTML += `<p>Name: ${peopleArray[i].name}</p>`;
+            peopleDisplay.innerHTML += `<p>Height: ${peopleArray[i].height}</p>`;
+            peopleDisplay.innerHTML += `<p>Hair Color: ${peopleArray[i].hair_color}</p>`;
+            peopleDisplay.innerHTML += `<p>----------------------------------</p>`;
+        } 
     });
-      
-      
